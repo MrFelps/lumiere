1069da6c00384cf0b8f852248c3d5eda24110e05
PedroHenriqueS19 <pedrohenriquef.sousa@outlook.com>
2026-04-10 16:49:52 -0300

    feat: criação da tela seleção de genêro de acordo com personalidade e criação da tela de notificações com filtros e gerenciamento de estado

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,11 +1,10 @@
-import logo from '../assets/logo-lumiere.png'
 import './style.css'; 
 
 function Header() {
   return (
     <header className="main-header">
       <div className="logo-container">
-        <img src={logo} alt="Lumiere Sun Logo" className="logo-img" />
+        <img src="/logo-lumiere.png" alt="Lumiere Sun Logo" className="logo-img" />
         <span className="logo-text">Lumière</span>
       </div>
     </header>
